0d5e2f44afd431739b94a105e8a7fc2fac16b29b
Hunter Benjamin <37448001+Chris-Return@users.noreply.github.com>
2026-04-01 12:50:34 +0200

    Autorisation d'écriture de pseudos en lowercase pour le remonter

--- commandes/InsulteIACommandInterpreter.py
+++ commandes/InsulteIACommandInterpreter.py
@@ -33,7 +33,7 @@ class InsulteIACommandInterpreter(CommandInterpreter):
         try:
             pique = self.gemini_bot.envoyer_message(prompt)
             if pique:
-                twSock.sendMessage(f"@{cible} {pique}")
+                twSock.sendMessage(f"@{username} {pique}")
                 
         except Exception as e:
             print(f"Erreur lors de la commande !insulte : {e}")
