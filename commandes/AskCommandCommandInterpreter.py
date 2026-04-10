904c16a975813ddcd884fd37fa591fd1e8035661
ChrisReturn <37448001+Chris-Return@users.noreply.github.com>
2026-04-10 17:24:52 +0200

    Ajout de la commande pour demander ce qu'on peut faire

---
+++ commandes/AskCommandCommandInterpreter.py
@@ -0,0 +1,13 @@
+from .CommandInterpreter import CommandInterpreter
+import time
+import random
+
+class AyaCommandInterpreter(CommandInterpreter):
+    def __init__(self, cooldown=30):
+        super().__init__(cooldown)
+        self.activationCommand = "!commande"
+
+    def execute(self, username, message, twSock):
+        msg = "Tu peux utiliser les commandes suivantes : !aya (compteur de aya) !cart (compteur de morts)... Bon ok c'est pas fou, juste mentionne moi et demande moi c'que tu veux si t'es un habitué du stream."
+        twSock.sendMessage(msg)
+        self.last_used = time.time()
