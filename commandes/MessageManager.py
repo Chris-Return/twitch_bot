fa1f7515b60ef468533af30545b54a78c141677f
ChrisReturn <37448001+Chris-Return@users.noreply.github.com>
2026-04-10 15:13:43 +0200

    Suppression de commandes qui entraînent un conflit

--- commandes/MessageManager.py
+++ commandes/MessageManager.py
@@ -2,9 +2,7 @@ from .AyaCommandInterpreter import AyaCommandInterpreter
 from .CartCommandInterpreter import CartCommandInterpreter
 from .ReveilCommandInterpreter import ReveilCommandInterpreter
 from .LurkCommandInterpreter import LurkCommandInterpreter
-from .InsulteIACommandInterpreter import InsulteIACommandInterpreter
 from .DanseCommandInterpreter import DanseCommandInterpreter
-from .LiveReactIACommandInterpreter import LiveReactIACommandInterpreter
 from IA.brain import IABrain 
 from IA.gemini import GeminiManager
 
@@ -15,9 +13,7 @@ class MessageManager:
                              CartCommandInterpreter(cooldown=10),
                              ReveilCommandInterpreter(cooldown=10),
                              LurkCommandInterpreter(cooldown=3),
-                             InsulteIACommandInterpreter(self.gemini_bot, cooldown=30),
                              DanseCommandInterpreter(cooldown=100),
-                             LiveReactIACommandInterpreter(self.gemini_bot, cooldown=0),
                              IABrain(self.gemini_bot)]
         
 
